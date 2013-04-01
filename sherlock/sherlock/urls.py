92d019bc0aa286ef3e85bf5926df7843cf937eb0
Eric Song <regonics@gmail.com>
2013-04-01 19:35:32 -0400

    added url catchers for authentication system

--- sherlock/sherlock/urls.py
+++ sherlock/sherlock/urls.py
@@ -1,17 +1,14 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sherlock.views.home', name='home'),
-    # url(r'^sherlock/', include('sherlock.foo.urls')),
+     url(r'^$', 'sherlockauth.views.login_user'),
+     url(r'^login/', 'sherlockauth.views.login_user'),
+     url(r'^logout/', 'sherlockauth.views.logout_user'),
+     url(r'^register/', 'sherlockauth.views.register_user'),
+     url(r'^home/', 'sherlockauth.views.homepage'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+     url(r'^admin/', include(admin.site.urls)),
 )
