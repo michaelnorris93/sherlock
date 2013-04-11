5c9bfb88c3366ec4f406a17b2f4deca807bf8241
Eric Song <regonics@gmail.com>
2013-04-11 01:36:22 -0400

    static serve working

--- sherlock/sherlock/urls.py
+++ sherlock/sherlock/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.conf import settings
+import os
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,4 +13,8 @@ urlpatterns = patterns('',
      url(r'^home/', 'sherlockauth.views.homepage'),
 
      url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),
 )
