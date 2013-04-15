672dd12706b47e181c83d06d29d031b30e0e43f9
Eric Song <regonics@gmail.com>
2013-04-15 15:08:32 -0400

    renamed module names in urls.py

--- sherlock/sherlock/urls.py
+++ sherlock/sherlock/urls.py
@@ -6,11 +6,11 @@ import os
 admin.autodiscover()
 
 urlpatterns = patterns('',
-     url(r'^$', 'sherlockauth.views.login_user'),
-     url(r'^login/', 'sherlockauth.views.login_user'),
-     url(r'^logout/', 'sherlockauth.views.logout_user'),
-     url(r'^register/', 'sherlockauth.views.register_user'),
-     url(r'^home/', 'sherlockauth.views.homepage'),
+     url(r'^$', 'auth.views.login_user'),
+     url(r'^login/', 'auth.views.login_user'),
+     url(r'^logout/', 'auth.views.logout_user'),
+     url(r'^register/', 'auth.views.register_user'),
+     url(r'^home/', 'auth.views.homepage'),
 
      url(r'^admin/', include(admin.site.urls)),
 )
