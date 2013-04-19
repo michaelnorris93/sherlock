94e8fd0c724b7e1e494c0e60bd81e0f738630285
Eric Song <regonics@gmail.com>
2013-04-19 05:53:06 -0400

    PlayGame now displays a random photo + a random question

--- sherlock/SherlockGame/views.py
+++ sherlock/SherlockGame/views.py
@@ -5,12 +5,23 @@ from auth.models import SherlockUser as SherlockUser_model
 from SherlockGame.models import QuestionType as QuestionType_model
 from SherlockGame.models import Answer as Answer_model
 
+import random
+
 @login_required(login_url="/login/")
 def PlayGame(request):
+    if request.POST:
+        if(request.POST.get('skipped') == 'True'):
+            #what to do if user skips question
+            #need to separately handle the case where the user skips before/after answering
+            #save the previous question appropriately and load a new one
+        else:
+            #what to do if user submits his answer
     num_photos = SherlockUser_model.objects.count() - SherlockUser_model.objects.filter(race=None).count()
-    s_userID = random.(1, num_photos)
-    photo_user = SherlockUser.objects.get(id=s_userID) #retrieve random SherlockUser 
+    s_userID = random.randint(1, num_photos)
+    photo_user = SherlockUser_model.objects.get(id=s_userID) #retrieve random SherlockUser 
 
-    num_questions = QuestionType_ 
+    num_questions = QuestionType_model.objects.filter(active=True).count() 
+    questionID = random.randint(1, num_questions)
+    questiontype = QuestionType_model.objects.get(id=questionID) #retrieve random question
 
-    return render(request, 'PlayGame.html', {'user': request.user, 'num': num_photos})
+    return render(request, 'PlayGame.html', {'user': request.user, 'photo_user': photo_user, 'question': questiontype})
